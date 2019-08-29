d0355f96208613b1e136e37ceb5bb65f95f86752
balumn <balumn@ieee.org>
2019-08-29 17:32:02 +0530

    hosting issues resolved

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ class Header extends Component {
         return (
             <div className="header">
                 <a href="/" className="logo"><h1 className="mh1">Thoughts OverFlow!</h1></a>
-                <h2 className="power"><a href="https://balumn.github.io" target="_blank" rel="noopener noreferrer"><img src="avatar.png" alt="@balumn" title="@balumn" /></a></h2>
+                <h2 className="power"><a href="https://balumn.github.io/blog" target="_blank" rel="noopener noreferrer"><img src="avatar.png" alt="@balumn" title="@balumn" /></a></h2>
             </div>
         );
     }
